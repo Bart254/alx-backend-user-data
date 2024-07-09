12830c3db8f7f821f49484923b2a799f5d8751cc
Bart254 <omondibartlett@gmail.com>
2024-07-10 00:26:49 +0300

    update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,7 +57,8 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
 
-        user_email, password = decoded_base64_authorization_header.split(':')
+        user_email, password = decoded_base64_authorization_header.split(':',
+                                                                         1)
         return (user_email, password)
 
     def user_object_from_credentials(
