cf06c1cd19a427b5d1fe7dd257da0a9aa9c626ec
Bart254 <omondibartlett@gmail.com>
2024-07-11 22:48:27 +0300

    update api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-    def user_id_for_session(self, session_id: str) -> str:
+    def user_id_for_session(self, session_id: str = None) -> str:
         """ returns user_id based on session_id """
         if session_id is None or not isinstance(session_id, str):
             return None
