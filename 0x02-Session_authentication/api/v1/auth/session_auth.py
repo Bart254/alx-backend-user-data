a7d6258f44cac7f0e7d5683680786c72ef2641fe
Bart254 <omondibartlett@gmail.com>
2024-07-11 22:54:50 +0300

    change function name in api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-    def user_id_for_session(self, session_id: str = None) -> str:
+    def user_id_for_session_id(self, session_id: str = None) -> str:
         """ returns user_id based on session_id """
         if session_id is None or not isinstance(session_id, str):
             return None
@@ -29,7 +29,7 @@ class SessionAuth(Auth):
     def current_user(self, request=None):
         """ returns user object based on session_id """
         session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session(session_id)
+        user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
 
@@ -41,7 +41,7 @@ class SessionAuth(Auth):
 
         if session_id is None:
             return False
-        user_id = self.user_id_for_session(session_id)
+        user_id = self.user_id_for_session_id(session_id)
 
         if user_id is None:
             return False
