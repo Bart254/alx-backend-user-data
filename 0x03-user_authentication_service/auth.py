50f049b319cc135ea54f7ab2b95753b4ac5661aa
Bart254 <omondibartlett@gmail.com>
2024-07-18 23:57:05 +0300

    update reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,7 +100,7 @@ class Auth:
         """updates user's reset token"""
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = self._generate_uuid()
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
 
