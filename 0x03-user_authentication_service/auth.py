6a762452022cba9aed57259620fe662ab04e1a0b
Bart254 <omondibartlett@gmail.com>
2024-07-18 23:15:13 +0300

    update comments

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,6 @@
 #!usr/bin/env python3
-"""Auth module"""
+"""Authentication module
+"""
 import bcrypt
 import uuid
 from db import DB
@@ -8,7 +9,14 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """returns salted hash of password"""
+    """returns salted hash of password
+
+    Args
+        password(str): password string
+
+    Returns:
+        bytes: encrypted password
+    """
     password_encoded = password.encode()
     hashed_password = bcrypt.hashpw(password_encoded, bcrypt.gensalt())
     return hashed_password
