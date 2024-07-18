951abd1b04c27437dd5931b2bc8a597829d65bc2
Bart254 <omondibartlett@gmail.com>
2024-07-18 23:59:11 +0300

    update reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -111,9 +111,8 @@ class Auth:
         """update's user password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            password = password.encode()
-            hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt()).decode()
-            self._db.update_user(user.id, hashed_password=hashed_pw,
+            hashed_password = _hash_password(password).decode()
+            self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
 
         except NoResultFound:
